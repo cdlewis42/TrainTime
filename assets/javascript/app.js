04122c021ea84ab38136c54d0532d4b64ef68aa7
Celeste Lewis <cdlewis42@gmail.com>
2020-02-05 09:33:19 -0500

    added css and some Javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,44 @@
-var tFrequency = 3;
-var firstTime = "03:30";
-var firstTimeConverted = moment(firstTime, "HH:mm").subttract(1,"years");
-var currentTime = moment()
+//Turns input into variable
+var table = $(".container")
+// On click appending all the data to the table
+$("#submit").on("click", function(event){
+    var frequency = $("#frequency").val().trim()
+
+    var firstTime = $("#firstTime").val().trim()
+
+    var destination = $("#destination").val().trim()
+
+    var name = $("#trainName").val().trim()
+
+    event.preventDefault();
+
+    var newCell1 = $("<td>")
+
+    newCell1.append(name)
+
+    var newCell2 =  $("<td>")
+
+    newCell2.append(destination) 
+
+    var newCell3 = $("<td>")
+
+
+    newCell3.append(firstTime)
+
+    var newCell4 = $("<td>")
+    newCell4.append(frequency)
+
+    
+    var newRow = $("<tr>")
+    newCell1.append(newRow)
+    newCell2.append(newRow)
+    newCell3.append(newRow)
+    newCell4.append(newRow)
+    newRow.append(table)
+//This is where I am emptying the input so it can have more information added.
+    frequency.empty()
+    firstTime.empty()
+    destination.empty()
+    name.empty()
+
+})
